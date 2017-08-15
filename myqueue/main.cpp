e6ebb5da39357348e35b098284f63c63e754cd05
fangdagang <429300406@qq.com>
2017-08-15 09:54:16 +0800

    20170815 09:54 am

--- myqueue/main.cpp
+++ myqueue/main.cpp
@@ -31,58 +31,100 @@ private:
     stack<int> s2;
 
     int length;
-    int * f;
-    int * b;
 };
 
-    MyQueue::MyQueue():length(0){}
-    MyQueue::~MyQueue(){}
+MyQueue::MyQueue():length(0){}
+MyQueue::~MyQueue(){}
 
-    bool MyQueue::empty()
+bool MyQueue::empty()
+{
+    if(length==0) return true;
+    else return false;
+}
+int MyQueue::size(){return length;}
+void MyQueue::push(int i)
+{
+    ++length; 
+    while(!s2.empty())
     {
-        if(length==0) return true;
-        else return false;
+        s1.push(s2.top());
+        s2.pop();
     }
-    int MyQueue::size(){return length};
-    void MyQueue::push(int i)
+    s2.push(i);
+}
+void MyQueue::pop()
+{
+    if(length<=0) return;
+    --length;
+    if(s1.empty()) 
     {
-        ++length; 
-        while(!s2.empty())
-        {
-            s1.push(s2.top());
-            s2.pop();
-        }
-        s2.push(i);
+        s2.pop();
+        return;
     }
-    void MyQueue::pop()
+    while(s1.size()!=1)
     {
-        --length;
-        if(s1.empty()) 
-        {
-            s2.pop();
-            return;
-        }
-        while(s1.size()!=1)
-        {
-            s2.push(s1.top());
-            s1.pop();
-        }
+        s2.push(s1.top());
         s1.pop();
     }
+    s1.pop();
+}
 
-    int MyQueue::front()
+int MyQueue::front()
+{
+    if(length==0) return 0;
+    if(s1.empty()) 
     {
-
-
+        return s2.top();
     }
-    int MyQueue::back();
+    while(s1.size()!=1)
+    {
+        s2.push(s1.top());
+        s1.pop();
+    }
+    return s1.top();
+
+}
+int MyQueue::back()
+{
+    if(length==0) return 0;
+    if(s2.empty()) return s1.top();
+    while(s2.size()!=1)
+    {
+        s1.push(s2.top());
+        s2.pop();
+    }
+    return s2.top();
+}
 
 
 
 int main()
 {
-
+    MyQueue mq;
     
+    cout<<"size1():"<<mq.size()<<endl;
+    
+    mq.push(1);
+    mq.push(2);
+    mq.push(3);
+    mq.push(4);
+    mq.push(5);
+    mq.push(6);
+    mq.push(7);
+    mq.push(8);
+    
+    cout<<"size2():"<<mq.size()<<endl;
+    cout<<"front2():"<<mq.front()<<endl;
+    cout<<"back2():"<<mq.back()<<endl;
+
+    mq.pop();
+    mq.pop();
+
+
+    cout<<"size3():"<<mq.size()<<endl;
+    cout<<"front3():"<<mq.front()<<endl;
+    cout<<"back3():"<<mq.back()<<endl;
+
 
       
     
